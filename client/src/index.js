e6454b0a14c7aaffd8037e2d33c0eb944e2acf6e
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-07-26 15:45:00 +0530

    Added Error Img

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import './index.css'
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter} from "react-router-dom"
+import ErrorImg from './imgs/e.jpg'
 import Home from "./views/Home/Home"
 import Login from './views/Login/Login';
 import Signup from './views/Signup/Signup'
@@ -21,7 +23,7 @@ const router = createBrowserRouter([
   },
   {
     path : "*",
-    element : <h1>Not Found</h1>
+    element : <img src={ErrorImg} alt='Error' className='error-img' />
   }
 ])
 
