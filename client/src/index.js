7085c3738b898d915940026c543594c7fc882c9e
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-07-26 14:01:12 +0530

    Added Login View

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter} from "react-router-dom"
 import Home from "./views/Home/Home"
+import Login from './views/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
   {
     path : "/",
     element : <Home />
+  },
+  {
+    path : "/login",
+    element : <Login />
   },
   {
     path : "*",
---
+++ client/src/views/Login/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import "./Login.css"
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const loginNow = async () => {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+      email: email,
+      password: password
+    })
+
+
+    if (response.data.success) {
+      toast.success(response.data.message)
+
+      localStorage.setItem('currentUser', JSON.stringify(response.data.data))
+
+      toast.loading('Redirecting to dashboard...')
+
+      setTimeout(() => {
+        window.location.href = '/'
+      }, 3000)
+    } else {
+      toast.error(response.data.message)
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <h2 >User Login</h2>
+
+      <div className='btn-container'>
+        <button type='button' className='login-btn'>Login</button>
+        <Link to='/signup' className='btn-regi'>Signup </Link>
+      </div>
+
+      <form >
+        <input
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type='button'
+          onClick={loginNow}
+          className='login-button'
+        >
+          Login
+        </button>
+      </form>
+
+      <Link to='/signup'className='a-bottom' >Don't have an account? Signup</Link>
+
+      <Toaster />
+    </div>
+  )
+}
+
+export default Login
